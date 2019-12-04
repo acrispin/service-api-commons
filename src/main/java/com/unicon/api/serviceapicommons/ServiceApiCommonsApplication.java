ce9d6735367b65213f9e80852627547e4211503a
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-04 13:01:54 -0500

    first commit

---
+++ src/main/java/com/unicon/api/serviceapicommons/ServiceApiCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.unicon.api.serviceapicommons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiCommonsApplication {
+
+}
---
+++ src/test/java/com/unicon/api/serviceapicommons/ServiceApiCommonsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.unicon.api.serviceapicommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test -----------");
+	}
+
+	@Test
+	void contextLoads2() {
+		System.out.println("test2 -----------");
+	}
+
+}
